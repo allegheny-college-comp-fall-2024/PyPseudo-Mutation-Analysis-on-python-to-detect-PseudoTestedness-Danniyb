b385276d9af8c282b7635e15a3e086e3f85bdc7e
Danniyb <bekele01@allegheny.edu>
2025-03-04 18:37:14 -0500

    feat: new test with mutant support struct

---
+++ pypseudo_instrumentation/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypseudo_instrumentation",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Support package for PyPseudo mutation testing",
+    author="Your Name",
+    python_requires=">=3.6",
+)
